2d1cac64d79330c9fcc022da3dd24528acf6635d
Andrew Rodko <rudmaster667@gmail.com>
2023-12-21 12:04:43 -0500

    Fix unit test assert from list count to list length

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -8,7 +8,7 @@ class UnitTestHandlerCase(unittest.TestCase):
     def test_response(self):
         print("Testing calls to lambda function")
         report_list = app.create_report_list()
-        self.assertIs(report_list.count(), REPORT_COUNT, "report list not returning correct count")
+        self.assertIs(len(report_list), REPORT_COUNT, "report list not returning correct count")
 
 
 if __name__ == '__main__':
